9ae1166d7664ea1f1f8f59b9dd8120c8223f77b6
plwdxiiu <plwdxiiu@gmail.com>
2019-11-17 17:43:46 +0800

    Delete loanSystem

--- loanSystem/loan/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-app_name = 'loan'
-urlpatterns = [
-    # url('', views.home, name='home'),
-    url('login/', views.login, name='login'),
-    url('aboutus/', views.aboutus, name='aboutus'),
-]
