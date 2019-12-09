03c17edaa81e8e4f4d5a994687fcf33304c1b72e
plwdxiiu <plwdxiiu@gmail.com>
2019-12-09 23:06:53 +0800

    部分applyForm

--- loanSystem/loan/static/apply/applyForm.js
+++ loanSystem/loan/static/apply/applyForm.js
@@ -1,37 +1,8 @@
-let nameInput = $("#name");
+let nameInput = $("#companyName");
 let codeInput = $("#creditCode");
 let flagOne = [false,false,false,false,false];
+let flagTwo = [false,false,false];
 
-nameInput.blur(function () {
-    if(nameInput.val() == null || nameInput.val() == ""){
-        nameInput.css("border","1px #f00 solid");
-        $("#nameHint").html("企业名称不能为空").css("color","red");
-        flagOne[0] = false;
-    }
-    else{
-        nameInput.css("border","1px #0f0 solid");
-        $("#nameHint").html("");
-        flagOne[0] = true;
-    }
-});
-
-codeInput.blur(function () {
-    if(codeInput.val() == null || codeInput.val() == ""){
-        codeInput.css("border","1px #f00 solid");
-        $("#codeHint").html("社会信用代码不能为空").css("color","red");
-        flagOne[1] = false;
-    }
-    else if(codeInput.length != 18){
-        codeInput.css("border","1px #f00 solid");
-        $("#codeHint").html("社会信用代码错误");
-        flagOne[1] = false;
-    }
-    else{
-        nameInput.css("border","1px #0f0 solid");
-        $("#codeHint").html("");
-        flagOne[1] = true;
-    }
-});
 
 if($("#stepOne").hasClass("active")){
     console.log(222);
@@ -51,56 +22,192 @@ if($("#stepThree").hasClass("active")){
     $("#stepFour").addClass("disabled");
 };
 
-$("#nextStep1").click(function () {
-    $("#step1").addClass("fade");
-    $("#step1").removeClass("active");
-    $("#step2").addClass("active");
-    $("#step2").addClass("show");
-    $("#stepOne").removeClass("active");
-    $("#stepTwo").addClass("active");
 
-    // $("#step1").addClass()
-});
-
-$("#nextStep2").click(function () {
-    $("#step2").addClass("fade");
-    $("#step2").removeClass("active");
-    $("#step3").addClass("active");
-    $("#step3").addClass("show");
-    $("#stepTwo").removeClass("disabled");
-    $("#stepTwo").removeClass("active");
-    $("#stepThree").addClass("active");
-});
-
-$("#nextStep3").click(function () {
-    $("#step3").addClass("fade");
-    $("#step3").removeClass("active");
-    $("#step4").addClass("active");
-    $("#step4").addClass("show");
-    $("#stepThree").removeClass("disabled");
-    $("#stepThree").removeClass("active");
-    $("#stepFour").addClass("active");
-});
-
-function upImage(target){
-    let fileSize = 0;
-    if(!target.files){
-        let filePath = target.value;
-        let fileSystem = new ActiveXObject("Scripting.FileSystemObject");
-        let file = fileSystem.GetFile(filePath);
-        fileSize = file.size;
+nameInput.blur(function nameCheck() {
+    if(nameInput.val() == null || nameInput.val() == ""){
+        nameInput.css("border","1px #f00 solid");
+        $("#nameHint").html("企业名称不能为空").css("color","red");
+        flagOne[0] = false;
     }
     else{
-        fileSize = target.files[0].size;
+        nameInput.css("border","1px #0f0 solid");
+        $("#nameHint").html("");
+        flagOne[0] = true;
     }
-    let size = fileSize/1024;
-    if(size > 0){
-        let txt = "";
-        window.wxc.xcConfirm(txt, window.wxc.xcConfirm.typeEnum.error);
-        $('.ok').click(function(){
-            window.location.reload();
-        });
+});
+
+codeInput.blur(function codeCheck() {
+    if(codeInput.val() == null || codeInput.val() == ""){
+        codeInput.css("border","1px #f00 solid");
+        $("#codeHint").html("社会信用代码不能为空").css("color","red");
+        flagOne[1] = false;
+    }
+    else if(codeInput.val().length != 18){
+        codeInput.css("border","1px #f00 solid");
+        $("#codeHint").html("社会信用代码错误");
+        flagOne[1] = false;
+    }
+    else{
+        codeInput.css("border","1px #0f0 solid");
+        $("#codeHint").html("");
+        flagOne[1] = true;
+    }
+});
+
+// 限制文件大小5M
+function checkfile(size,i) {
+    let hint = `#${baseRes[i]}Hint`;
+    const isLt2M = size / 1024 / 1024 < 5;
+    if(size == "" || size == null || size == 0){
+        $(hint).html("文件未上传");
+        $(hint).css("color","#f00");
+        // message.error('文件超过5M限制，请重新上传');
+        return false;
+    }
+    if (!isLt2M) {
+        $(hint).html("文件超过5M限制，请重新上传");
+        $(hint).css("color","#f00");
+        return false;
+    }
+    else{
+        $(hint).html("文件上传成功");
+        $(hint).css("color","#0f0");
+        return true;
     }
 }
+
+//存储所有数据的数组
+let allDatas = new Array();
+
+//基础材料(第一个表单)的id、name名称
+let baseRes = ['companyName','creditCode','license','taxRegist','companyCredit'];
+
+//基础材料(第一个表单)校验及跳转
+$("#nextStep1").click(function () {
+    //由于是第一步 所有的文件需要重新上传 所以将总数据清空 重新存储
+    allDatas = [];
+    let datas = new Array();
+    let form = document.getElementById("companyForm");
+    let formdata = new FormData(form);
+
+    //将form中的值传到添加到datas中
+    for(let i in baseRes) {
+        datas[i] = formdata.get(baseRes[i]);
+    }
+
+    // 校验文件大小
+    for(let i = 2; i < 5; i++){
+        if(checkfile(datas[i].size,i)){
+            flagOne[i] = true;
+        }
+    }
+    console.log(flagOne);
+
+    if(flagOne[0] == true && flagOne[1] == true && flagOne[2] == true
+        && flagOne[3] == true && flagOne[4] == true){
+
+        allDatas.push(datas);
+
+        $("#step1").addClass("fade");
+        $("#step1").removeClass("active");
+        $("#step2").addClass("active");
+        $("#step2").addClass("show");
+        $("#stepOne").removeClass("active");
+        $("#stepTwo").addClass("active");
+    }
+    else{
+        let txt = "信息有误";
+        window.wxc.xcConfirm(txt, window.wxc.xcConfirm.typeEnum.error);
+        // $('.ok').click(function(){
+        //     window.location.reload();
+        // });
+    }
+
+});
+
+let financeRes = ['finance','assets','companyCredit'];
+
+//财务资料(第二个表单)校验及跳转
+$("#nextStep2").click(function () {
+    let datas = new Array();
+    let form2 = document.getElementById("financeForm");
+    let formdata = new FormData(form2);
+
+    //将form中的值传到添加到datas中
+    for(let i in financeRes) {
+        datas[i] = formdata.get(financeRes[i]);
+    }
+
+    // 校验文件大小
+    for(let i = 0; i < 3; i++){
+        if(checkfile(datas[i].size,i)){
+            flagTwo[i] = true;
+        }
+    }
+    console.log(flagTwo);
+
+    if(flagTwo[0] == true && flagTwo[1] == true && flagTwo[2] == true){
+        allDatas.push(datas);
+
+        $("#step2").addClass("fade");
+        $("#step2").removeClass("active");
+        $("#step3").addClass("active");
+        $("#step3").addClass("show");
+        $("#stepTwo").removeClass("disabled");
+        $("#stepTwo").removeClass("active");
+        $("#stepThree").addClass("active");
+    }
+    else{
+        let txt = "信息有误";
+        window.wxc.xcConfirm(txt, window.wxc.xcConfirm.typeEnum.error);
+        // $('.ok').click(function(){
+        //     window.location.reload();
+        // });
+    }
+
+});
+
+
+
+$("#nextStep3").click(function () {
+    let datas = new Array();
+    let form = document.getElementById("financeForm");
+    let formdata = new FormData(form);
+
+    //将form中的值传到添加到datas中
+    for(let i in financeRes) {
+        datas[i] = formdata.get(financeRes[i]);
+    }
+
+    // 校验文件大小
+    for(let i = 0; i < 3; i++){
+        if(checkfile(datas[i].size,i)){
+            flagTwo[i] = true;
+        }
+    }
+    console.log(flagTwo);
+
+    if(flagTwo[0] == true && flagTwo[1] == true && flagTwo[2] == true){
+        allDatas.push(datas);
+
+        $("#step3").addClass("fade");
+        $("#step3").removeClass("active");
+        $("#step4").addClass("active");
+        $("#step4").addClass("show");
+        $("#stepThree").removeClass("disabled");
+        $("#stepThree").removeClass("active");
+        $("#stepFour").addClass("active");
+    }
+    else{
+        let txt = "信息有误";
+        window.wxc.xcConfirm(txt, window.wxc.xcConfirm.typeEnum.error);
+        // $('.ok').click(function(){
+        //     window.location.reload();
+        // });
+    }
+
+
+});
+
 
 
