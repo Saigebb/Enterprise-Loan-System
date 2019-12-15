f4229c17687c5884e08ab9fb887407506b796a1d
shiyingnan1998 <2597795315@qq.com>
2019-12-15 14:59:24 +0800

    syn提交2

--- loanSystem/loan/static/loanManage/loanManage.js
+++ loanSystem/loan/static/loanManage/loanManage.js
@@ -1,15 +1,22 @@
 //搜索
-$("#search_btn").click(function () {
-    $("#loanManage_tbody").html('');
-    data = {}
-    data.search_name = $("#search_name").val();
-    // console.log($("#search_name").val());
-    $.post("/manage_search/", data, function (data) {
-        if (data == "0") {
+// $("#search_btn").click(function () {
+//     $("#loanManage_tbody").html('');
+//     data = {};
+//     data.name = $("#search_name").val();
+//     // console.log($("#search_name").val());
+//     $.post("/loanManage/", data, function (data) {
+//         // if (data == "0") {
+//         //     // 弹框
+//         //     let txt = "搜索失败";
+//         //     window.wxc.xcConfirm(txt, window.wxc.xcConfirm.typeEnum.error);
+//         //     $('.ok').click(function () {
+//         //         window.location.reload();
+//         //     });
+//         // }
+//     });
 
-        }
-    });
-})
+
+// })
 
 
 
